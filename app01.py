ef1f8edd165cdd5ac53bb05c3b820894505a3115
Daienne Smith <daienne.pma@gmail.com>
2024-10-15 11:49:11 -0300

    aula de introdução

---
+++ app01.py
@@ -0,0 +1,20 @@
+#Crie uma variável usando "snake_case"
+cor_da_casa = 'azul'
+
+#crie uma variável usando "Camel Case"
+corDaCasa = "amarela"
+
+#Exibe as coisas
+print('\n\n\n')
+print(cor_da_casa)
+print(corDaCasa)
+
+#variável para primeiro nome
+primeiro_nome = "Daienne"
+ultimo_nome = "Ferreira"
+
+print(primeiro_nome)
+print(ultimo_nome)
+
+#para imprimir os dois nomes juntos
+print(primeiro_nome,ultimo_nome)
